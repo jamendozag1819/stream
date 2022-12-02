99ceb07b64e33930aeaedf74543fb05d55baf63f
RicardoAzcar <rfazcar@minsait.com>
2022-12-02 11:02:59 -0600

    Instrucciones

---
+++ src/main/java/Tarea1Stream/Coleccion.java
@@ -0,0 +1,32 @@
+package Tarea1Stream;
+
+import java.util.ArrayList;
+import java.util.List;
+// Y la siguiente coleccion
+public class Coleccion {
+    public static void main(String[] args) {
+        List<Curso> cursos = new ArrayList<>();
+        cursos.add(new Curso("Cursos profesional de Java", 6.5f, 50, 200 ));
+        cursos.add(new Curso("Cursos profesional de Python", 8.5f, 60, 800 ));
+        cursos.add(new Curso("Cursos profesional de DB", 4.5f, 70, 700 ));
+        cursos.add(new Curso("Cursos profesional de Android", 7.5f, 10, 400 ));
+        cursos.add(new Curso("Cursos profesional de Escritura", 1.5f, 10, 300));
+    }
+    /***
+     * Resolver los siguientes problemas:
+     * Obtener la cantidad de cursos con una duración mayor a 5 horas.
+     * Obtener la cantidad de cursos con una duración menor a 2 horas.
+     * Listar el título de todos aquellos cursos con una cantidad de vídeos mayor
+     * a 50.
+     * Mostrar en consola el título de los 3 cursos con mayor duración.
+     * Mostrar en consola la duración total de todos los cursos.
+     * Mostrar en consola todos aquellos libros que superen el promedio en
+     * cuanto a duración se refiere.
+     * Mostrar en consola la duración de todos aquellos cursos que tengan una
+     * cantidad de alumnos inscritos menor a 500.
+     * Obtener el curso con mayor duración.
+     * Crear una lista de Strings con todos los titulos de los cursos.
+     *
+     * Una vez terminado subirlo al repositorio
+     */
+}
